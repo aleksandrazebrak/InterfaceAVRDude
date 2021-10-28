2a4b28365e2f8d8e3c5bce5ac321a99ee9a7d70f
Aleksa <48763023+aleksandrazebrak@users.noreply.github.com>
2021-10-28 14:39:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
+
 #include <QApplication>
 #include <QDir>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //ustawienie ścieżki do aplikacji
     QDir::setCurrent(a.applicationDirPath());
+
     MainWindow w;
     w.show();
     return a.exec();
 }
+
